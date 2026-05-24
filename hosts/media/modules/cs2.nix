060f56861131a761df950b3baf69336b5e3b97e5
Thomas Petersson <thomas@petersson.priv.no>
2026-05-24 13:42:49 +0200

    move competitive.cfg over

--- hosts/media/modules/cs2.nix
+++ hosts/media/modules/cs2.nix
@@ -47,6 +47,8 @@ in {
     # cfg/custom_all.cfg is execed after server.cfg for every gamemode.
     "${pkgs.coreutils}/bin/install -d -o games -g games -m 2770 ${cs2CustomDir}/cfg"
     "${pkgs.coreutils}/bin/install -m 0660 -o games -g games ${./cs2-presets/custom_all.cfg} ${cs2CustomDir}/cfg/custom_all.cfg"
+    # Move our competitive config over
+		"${pkgs.coreutils}/bin/install -m 0660 -o games -g games ${./cs2-presets/competitive.cfg} ${cs2CustomDir}/cfg/competitive.cfg"
   ];
 
   # kus image env var names. API_KEY required for workshop downloads — get one
