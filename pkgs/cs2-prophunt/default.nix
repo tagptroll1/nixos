0596d459a2da33394f767dad49b89d0b212c1886
Thomas Petersson <thomas@petersson.priv.no>
2026-06-01 22:03:58 +0200

    hash

--- pkgs/cs2-prophunt/default.nix
+++ pkgs/cs2-prophunt/default.nix
@@ -32,7 +32,7 @@ buildDotnetModule (finalAttrs: {
     # below is upstream's last commit (will fail because the fork's main has
     # since diverged once you push). Use lib.fakeHash to TOFU the hash.
     rev = "f3ef9d43dfd54ff1f80340332a20452e7655e110";
-    hash = lib.fakeHash;
+    hash = "sha256-kyLRmYqwRKd3AaUytn6W2N3WOM+6OOo9bliTeFRnv2M=";
   };
 
   projectFile = "src/PropHunt.csproj";
